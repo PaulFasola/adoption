0f56c23518e768fdf5402e526ca9b898bb9c1bb7
paul.fasola@mindgeek.com <paul.fasola@gmail.com>
2021-03-01 00:02:07 -0500

    Added a dead simple example project to ensure that the build is functional

---
+++ example/webpack.config.js
@@ -0,0 +1,32 @@
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.tsx?$/],
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.ts', '.tsx', '.js'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+      'react': path.resolve('./node_modules/react')
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js',
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'),
+    hot: true,
+  },
+};
