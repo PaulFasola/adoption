7298adb6a68ecb15033877fe6a4cd1dcdc7841ff
PaulFasola <PaulFasola@users.noreply.github.com>
2021-03-02 07:11:06 +0000

    Deploying to gh-pages from @ PaulFasola/adoption@24b3912532d03c9f5c297d4916496f5f8879a517 🚀

--- README.md
+++
@@ -1,3 +0,0 @@
-# Adoption dApp UI Framework
-
-[![Github Actions](https://github.com/PaulFasola/adoption/actions/workflows/actions.yaml/badge.svg)](https://github.com/PaulFasola/adoption/actions/workflows/actions.yaml)
--- src/components/TransactionStatus/index.stories.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { TransactionStatus } from '.';
-
-storiesOf('Transaction Status', module)
-  .add('Default', () => <TransactionStatus />);
