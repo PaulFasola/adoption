bede6bc593f9979c7921630ccc011633c61fea7b
paul.fasola@mindgeek.com <paul.fasola@gmail.com>
2021-03-02 02:06:19 -0500

    [Icon] Initial implementation

---
+++ src/components/common/Icon/style.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Link = styled.a`
+	color: rgba(0, 0, 0, 0.8);
+	text-decoration: none;
+`;
+
+export const IconContainer = styled.div`
+	margin: 5px 3px 3px 3px;
+	display: inline-block;
+    vertical-align: middle;
+
+	svg .fillable {
+		fill: black;
+	}
+`;
