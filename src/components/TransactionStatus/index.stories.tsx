743b23d5a7767d9c910d67c6c5760b9aad32dffe
Paul Fasola <paul.fasola@gmail.com>
2021-03-05 03:59:26 -0500

    docs(storybook): slight cosmetic changes

--- src/components/TransactionStatus/index.stories.tsx
+++ src/components/TransactionStatus/index.stories.tsx
@@ -5,11 +5,11 @@ import { IAdress } from './interfaces';
 import { TxStatus } from './txStatus';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const txCompleted = require('../../assets/txstatus-completed.jpg')
+const txCompleted = require('../../assets/txstatus-completed.jpg');
 
 export default {
   title: 'TransactionStatus',
-  component: TransactionStatus
+  component: TransactionStatus,
 };
 
 const Spacer = styled.div`
@@ -20,44 +20,44 @@ export const Basic = (): React.ReactNode => {
   return (
     <TransactionStatus
       status={TxStatus.COMPLETED}
-      amount="0.1"
-      symbol="BTC"
-      txURL="https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1"
+      amount='0.1'
+      symbol='BTC'
+      txURL='https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1'
     />
-  )
+  );
 };
 
 export const Statuses = (): React.ReactNode => (
   <Fragment>
     <Spacer>
       <TransactionStatus
-        amount="0.1"
-        symbol="BTC"
-        txURL="https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1"
+        amount='0.1'
+        symbol='BTC'
+        txURL='https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1'
       />
     </Spacer>
     <Spacer>
       <TransactionStatus
-        amount="0.1"
-        symbol="BTC"
+        amount='0.1'
+        symbol='BTC'
         status={TxStatus.PENDING}
-        txURL="https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1"
+        txURL='https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1'
       />
     </Spacer>
     <Spacer>
       <TransactionStatus
-        amount="0.1"
-        symbol="BTC"
+        amount='0.1'
+        symbol='BTC'
         status={TxStatus.COMPLETED}
-        txURL="https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1"
+        txURL='https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1'
       />
     </Spacer>
     <Spacer>
       <TransactionStatus
-        amount="0.1"
-        symbol="BTC"
+        amount='0.1'
+        symbol='BTC'
         status={TxStatus.FAILED}
-        txURL="https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1"
+        txURL='https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1'
       />
     </Spacer>
   </Fragment>
@@ -66,25 +66,23 @@ export const Statuses = (): React.ReactNode => (
 export const TransactionDetails = (): React.ReactNode => {
   return (
     <TransactionStatus
-      amount="0.1"
-      symbol="BTC"
-      txFees="0.0001"
+      amount='0.1'
+      symbol='BTC'
+      txFees='0.0001'
       date={{
-        value: new Date()
+        value: new Date(),
       }}
       sender={{
-        hash: "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa",
-        url: "https://www.blockchain.com/btc/address/1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa"
+        hash: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa',
+        url: 'https://www.blockchain.com/btc/address/1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa',
       }}
       receiver={{
-        hash: "1CounterpartyXXXXXXXXXXXXXXXUWLpVr"
+        hash: '1CounterpartyXXXXXXXXXXXXXXXUWLpVr',
       }}
-      customDetailComponent={
-        <b>Hello, this is a custom component!</b>
-      }
-      txURL="https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1"
+      customDetailComponent={<b>Hello, this is a custom component!</b>}
+      txURL='https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1'
     />
-  )
+  );
 };
 
 export const Animated = (): React.ReactNode => (
@@ -92,37 +90,37 @@ export const Animated = (): React.ReactNode => (
     <Spacer>
       <TransactionStatus
         animated
-        amount="0.1"
-        symbol="BTC"
+        amount='0.1'
+        symbol='BTC'
         status={TxStatus.PENDING}
-        txURL="https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1"
+        txURL='https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1'
       />
     </Spacer>
     <Spacer>
       <TransactionStatus
         animated
-        amount="0.1"
-        symbol="BTC"
+        amount='0.1'
+        symbol='BTC'
         status={TxStatus.UNKNOWN}
-        txURL="https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1"
+        txURL='https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1'
       />
     </Spacer>
     <Spacer>
       <TransactionStatus
         animated
-        amount="0.1"
-        symbol="BTC"
+        amount='0.1'
+        symbol='BTC'
         status={TxStatus.COMPLETED}
-        txURL="https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1"
+        txURL='https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1'
       />
     </Spacer>
     <Spacer>
       <TransactionStatus
         animated
-        amount="0.1"
-        symbol="BTC"
+        amount='0.1'
+        symbol='BTC'
         status={TxStatus.FAILED}
-        txURL="https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1"
+        txURL='https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1'
       />
     </Spacer>
   </Fragment>
@@ -136,24 +134,28 @@ export const Simulation: React.FC = () => {
 
   const timer = useRef<NodeJS.Timeout>();
 
-  const [isInit, setIsInit] = useState<boolean>(false);
   const [status, setStatus] = useState<TxStatus>(TxStatus.PENDING);
+  const [tx, setTx] = useState<Partial<ITransaction>>({});
+  const [isInit, setIsInit] = useState<boolean>(false);
   const [txURL, setTxURL] = useState<string>();
 
-  const [tx, setTx] = useState<Partial<ITransaction>>({})
+  const _clearTimeout = () => timer.current && clearInterval(timer.current);
+  const _delay = (delay: number): Promise<void> =>
+    new Promise((resolve) => setTimeout(resolve, delay));
 
-  const _clearTimeout = () => timer.current && clearInterval(timer.current);;
-  useEffect(() => { return () => _clearTimeout() }, [])
-
-  const _delay = (delay: number): Promise<void> => new Promise(function (resolve) {
-    setTimeout(resolve, delay);
-  });
+  useEffect(() => {
+    return () => _clearTimeout();
+  }, []);
 
   const runSimulation = (): void => {
     _clearTimeout();
 
     _delay(3 * 1000)
-      .then(() => setTxURL('https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1'))
+      .then(() =>
+        setTxURL(
+          'https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1'
+        )
+      )
 
       // wait 5s before completing that transaction
       .then(() => _delay(5 * 1000))
@@ -161,39 +163,40 @@ export const Simulation: React.FC = () => {
 
       // let's say, tx infos need to be fetched before being able to render them
       .then(() => _delay(1.5 * 1000))
-      .then(() => setTx({
-        ...tx, ...{
-          sender: {
-            hash: "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa",
-            url: "https://www.blockchain.com/btc/address/1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa"
+      .then(() =>
+        setTx({
+          ...tx,
+          ...{
+            sender: {
+              hash: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa',
+              url: 'https://www.blockchain.com/btc/address/1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa',
+            },
+            receiver: {
+              hash: '1CounterpartyXXXXXXXXXXXXXXXUWLpVr',
+              url: 'https://www.blockchain.com/btc/address/1CounterpartyXXXXXXXXXXXXXXXUWLpVr',
+            },
+            txFees: '0.0001',
+            customDetailComponent: (
+              <img src={txCompleted} alt='Custom component' style={{ width: '100%' }} />
+            ),
           },
-          receiver: {
-            hash: "1CounterpartyXXXXXXXXXXXXXXXUWLpVr",
-            url: "https://www.blockchain.com/btc/address/1CounterpartyXXXXXXXXXXXXXXXUWLpVr"
-          },
-          txFees: "0.0001",
-          customDetailComponent: (
-            <img src={txCompleted} alt="Custom component" style={{ width: '100%' }} />
-          )
-        }
-      }));
-  }
+        })
+      );
+  };
 
   const _handleReset = (): void => {
     setTx({});
     setStatus(TxStatus.PENDING);
     runSimulation();
-  }
+  };
 
   const _handleInit = (): void => {
     setIsInit(true);
     runSimulation();
-  }
+  };
 
   if (!isInit) {
-    return (
-      <button onClick={_handleInit}>Begin transaction</button>
-    )
+    return <button onClick={_handleInit}>Begin transaction</button>;
   }
 
   return (
@@ -201,22 +204,25 @@ export const Simulation: React.FC = () => {
       <TransactionStatus
         animated
         autoShowDetails
-        amount="0.1"
-        symbol="BTC"
+        amount='0.1'
+        symbol='BTC'
         status={status}
         txURL={txURL}
         date={{
-          value: new Date()
+          value: new Date(),
         }}
         {...tx}
       />
-      <button style={{ marginTop: '10px' }} onClick={_handleReset}>Reset</button>
+      <button style={{ marginTop: '10px' }} onClick={_handleReset}>
+        Reset
+      </button>
       <i style={{ display: 'block', fontSize: '12px', marginTop: '10px' }}>
         <p>
           In this example, <u>autoShowDetails</u> property is added. <br />
-          When the component will receive props, it will automatically expand the card. This is fully optional.
+          When the component will receive props, it will automatically expand the card. This is
+          fully optional.
         </p>
       </i>
     </Spacer>
-  )
+  );
 };
