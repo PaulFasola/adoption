d9fc200d52ed0a5e276702a070529bebd936bc5a
Paul Fasola <paul.fasola@gmail.com>
2021-03-06 05:09:58 -0500

    style(animatedicon): migrates to dynamic theming

--- src/components/common/AnimatedIcon/__tests__/index.test.tsx
+++ src/components/common/AnimatedIcon/__tests__/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import toJson from "enzyme-to-json";
+import toJson from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
 import { configure, shallow } from 'enzyme';
 import { AnimatedIcon, IconType } from '..';
@@ -8,16 +8,11 @@ configure({ adapter: new Adapter() });
 
 describe(`AnimatedIcon`, () => {
   it('Should render', () => {
-    Object
-      .keys(IconType)
-      .forEach((key) => shallow(
-        <AnimatedIcon type={IconType[key]} />
-      ));
+    Object.keys(IconType).forEach((key) => shallow(<AnimatedIcon type={IconType[key]} />));
   });
 
   it('Should be styleable', () => {
     const wrapper = shallow(<AnimatedIcon type={IconType.Sucess} />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
-
 });
