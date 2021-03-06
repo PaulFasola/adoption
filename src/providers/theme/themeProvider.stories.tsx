2b5a43e9879896e24f2facb575343c13ea37ff56
Paul Fasola <paul.fasola@gmail.com>
2021-03-06 05:13:35 -0500

    docs(themeprovider): stories initial implementation

--- src/providers/theme/themeProvider.stories.tsx
+++ src/providers/theme/themeProvider.stories.tsx
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react';
-import { PaymentRequest } from '../../components/PaymentRequest';
 import { PaymentStatus } from '../../components/PaymentRequest/enums/paymentStatus';
-import { ITheme } from './ITheme';
+import { TransactionStatus, TxStatus } from '../../components/TransactionStatus';
+import { PaymentRequest } from '../../components/PaymentRequest';
 import { ThemeContext, ThemeProvider } from './themeProvider';
+import { ITheme } from './ITheme';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const companyAsset = require('../../assets/fictiveCompany.png');
@@ -13,45 +14,54 @@ export default {
 
 const LabRat = (): React.ReactElement => {
   return (
-    <PaymentRequest
-      symbol='BTC'
-      decimalPlaces={8}
-      sellerName='Such Company LTD'
-      logos={{
-        coin: 'https://upload.wikimedia.org/wikipedia/commons/c/c5/Bitcoin_logo.svg',
-        company: companyAsset,
-      }}
-      address='1BitcoinEaterAddressDontSendf59kuE'
-      amount={{ toPay: 0.9, received: 0.2 }}
-      transactions={[
-        {
-          txHash: '369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1',
-          txUrl:
-            'https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1',
-          amount: 0.1,
-        },
-        {
-          txHash: '519f6c9581ce27e0a59f5f8e427b672087e1f2eb1aead0d66288de62ed3e9647',
-          txUrl:
-            'https://www.blockchain.com/btc/tx/519f6c9581ce27e0a59f5f8e427b672087e1f2eb1aead0d66288de62ed3e9647',
-          amount: 0.1,
-        },
-      ]}
-      helpUrl='https://github.com/PaulFasola/adoption/blob/master/README.md'
-      deadline={{
-        dateLocale: 'en-US',
-        datetime: new Date(),
-        humanized: true,
-      }}
-      status={PaymentStatus.COMPLETE}
-      strings={{
-        txStatus: {
-          failed: 'Payment failed :(',
-          pending: 'Waiting for payment',
-          complete: 'Paid! Redirecting...',
-        },
-      }}
-    />
+    <>
+      <TransactionStatus
+        status={TxStatus.PENDING}
+        amount='0.1'
+        symbol='BTC'
+        txURL='https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1'
+      />
+      <br />
+      <PaymentRequest
+        symbol='BTC'
+        decimalPlaces={8}
+        sellerName='Such Company LTD'
+        logos={{
+          coin: 'https://upload.wikimedia.org/wikipedia/commons/c/c5/Bitcoin_logo.svg',
+          company: companyAsset,
+        }}
+        address='1BitcoinEaterAddressDontSendf59kuE'
+        amount={{ toPay: 0.9, received: 0.2 }}
+        transactions={[
+          {
+            txHash: '369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1',
+            txUrl:
+              'https://www.blockchain.com/btc/tx/369d241af595fc253479abe394e2f21fda05820a0416942f63266dd793035cf1',
+            amount: 0.1,
+          },
+          {
+            txHash: '519f6c9581ce27e0a59f5f8e427b672087e1f2eb1aead0d66288de62ed3e9647',
+            txUrl:
+              'https://www.blockchain.com/btc/tx/519f6c9581ce27e0a59f5f8e427b672087e1f2eb1aead0d66288de62ed3e9647',
+            amount: 0.1,
+          },
+        ]}
+        helpUrl='https://github.com/PaulFasola/adoption/blob/master/README.md'
+        deadline={{
+          dateLocale: 'en-US',
+          datetime: new Date(),
+          humanized: true,
+        }}
+        status={PaymentStatus.COMPLETE}
+        strings={{
+          txStatus: {
+            failed: 'Payment failed :(',
+            pending: 'Waiting for payment',
+            complete: 'Paid! Redirecting...',
+          },
+        }}
+      />
+    </>
   );
 };
 
@@ -63,7 +73,12 @@ export const Presets: React.FC = () => {
       <>
         <div style={{ marginBottom: '20px' }}>
           <p>
-            Theme is <b>{currentTheme.name}</b>.
+            Theme is <b>{currentTheme.name}</b>
+            <br />
+            <small>
+              It&apos;s saved into your browser&apos;s localStorage, so this theme will persist if
+              you refresh the page.
+            </small>
           </p>
           <button onClick={() => switchTo('dark')}>Go dark</button>
           <button onClick={() => switchTo('light')}>Go light</button>
@@ -80,12 +95,18 @@ export const Presets: React.FC = () => {
   );
 };
 
-export const CustomPresets: React.FC = () => {
-  const customThemes: Record<string, ITheme> = {
+export const CustomThemes: React.FC = () => {
+  const customThemes: Record<string, Partial<ITheme>> = {
     wow: {
       primary: {
         backgroundColor: 'pink',
-        color: 'purple',
+        color: 'yellow',
+      },
+    },
+    veryOrganic: {
+      primary: {
+        backgroundColor: 'lightgreen',
+        color: 'darkgreen',
       },
     },
   };
@@ -93,11 +114,25 @@ export const CustomPresets: React.FC = () => {
   const MyPaymentRequest: React.FC = () => {
     const { currentTheme, switchTo } = useContext(ThemeContext);
     const availableThemes = [...Object.keys(customThemes), ...['light', 'dark']];
+
     return (
       <>
         <div style={{ marginBottom: '20px' }}>
           <p>
-            Theme is <b>{currentTheme.name}</b>.
+            Theme is <b>{currentTheme.name}</b>.<br />
+            <ul>
+              <li>
+                You can fully (or even partially) create your themes and enable them simply by
+                proving these ones to the <i>&lt;ThemeProvider /&gt;.</i>
+              </li>
+              <li>
+                The chosen theme is saved into your browser&apos;s localStorage, so this theme will
+                persist if you refresh the page.
+              </li>
+              <li>
+                Also, you can trigger page&apos;s dark mode on Storybook, check the button above ☝️
+              </li>
+            </ul>
           </p>
           {availableThemes.map((key, i) => (
             <button key={i.toString()} onClick={() => switchTo(key)}>
