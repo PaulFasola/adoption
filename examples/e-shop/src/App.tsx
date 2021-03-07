b1ccd5b0768d26921b080333fb94f48fcc352342
Paul Fasola <paul.fasola@gmail.com>
2021-03-06 23:51:53 -0500

    chore(e-shop): moved modal to the root

--- examples/e-shop/src/App.tsx
+++ examples/e-shop/src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { makeStyles } from '@material-ui/core';
+import { Modal } from '@material-ui/core';
 import { hot } from 'react-hot-loader';
 import { Container } from '@material-ui/core';
 import { Header } from './components/header';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { PaymentProcess } from './components/paymentProcess';
 import { routeMap } from './router-paths';
+import { modalState } from './atoms';
 
 import './style.scss';
 
+const useStyles = makeStyles(() => ({
+  modal: { display: 'flex', alignItems: 'center', justifyContent: 'center' },
+}));
+
 export const App = hot(module)(() => {
+  const [isModalOpen, setIsModalOpen] = useRecoilState<boolean>(modalState);
+  const styles = useStyles();
+
   return (
     <BrowserRouter>
       <Header />
@@ -19,7 +31,19 @@ export const App = hot(module)(() => {
             </Route>
           ))}
         </Switch>
+
+        <Modal
+          className={styles.modal}
+          open={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          aria-labelledby='Checkout Modal'
+          aria-describedby='Proceed to checkout, prepare your cryptos!'
+        >
+          <div>
+            <PaymentProcess />
+          </div>
+        </Modal>
       </Container>
     </BrowserRouter>
-  )
+  );
 });
