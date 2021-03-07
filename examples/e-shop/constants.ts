7c129117bd232c96585bd94f4f57ede0981d379f
Paul Fasola <paul.fasola@gmail.com>
2021-03-07 03:36:30 -0500

    chore(e-shop): regrouped constant strings

---
+++ examples/e-shop/constants.ts
@@ -0,0 +1,12 @@
+// Logos
+import coinLogo from './assets/woofLogo.jpg';
+import companyLogo from './assets/companyLogo.png';
+
+export const constants = {
+  coin: 'WOOF',
+  dataEndpoint: 'https://dog.ceo/api/breeds/image/random/30',
+  assets: {
+    coin: coinLogo,
+    company: companyLogo,
+  },
+};
