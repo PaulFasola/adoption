0706f7585ebb3eeba38dd5a0f898f68f9c5c2888
Paul Fasola <paul.fasola@gmail.com>
2021-03-06 23:48:31 -0500

    chore(e-shop): header implementation

--- examples/e-shop/src/components/header/index.tsx
+++ examples/e-shop/src/components/header/index.tsx
@@ -1,72 +1,100 @@
-import React from "react"
-import { AppBar, Toolbar, IconButton, Typography, SwipeableDrawer, makeStyles, List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core"
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  SwipeableDrawer,
+  makeStyles,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@material-ui/core';
 import { Receipt, Storefront, Menu, Favorite } from '@material-ui/icons';
-import { useHistory } from "react-router-dom";
-import { Path } from "../../router-paths";
+import { useHistory } from 'react-router-dom';
+import { Path } from '../../router-paths';
+import { useRecoilState } from 'recoil';
+import { headerState } from '../../atoms';
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		flexGrow: 1,
-	},
-	menuButton: {
-		marginRight: theme.spacing(2),
-	},
-	title: {
-		flexGrow: 1,
-	},
-	list: {
-		width: 250,
-	},
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  list: {
+    width: 250,
+  },
 }));
 
 export const Header: React.FC = () => {
-	const history = useHistory();
-	const classes = useStyles();
-	const [isDrawerOpen, setIsDrawerOpen] = React.useState<boolean>(false);
+  const history = useHistory();
+  const classes = useStyles();
 
-	const toggleDrawer = (isOpen: boolean) => {
-		console.log({ isOpen });
-		setIsDrawerOpen(isOpen);
-	};
+  const [header] = useRecoilState(headerState);
+  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
 
-	const _navigateTo = (localPath: string) => (): void => {
-		history.push(localPath);
-		setIsDrawerOpen(false);
-	}
+  const toggleDrawer = (isOpen: boolean) => {
+    console.log({ isOpen });
+    setIsDrawerOpen(isOpen);
+  };
 
-	return (
-		<>
-			<AppBar position="static">
-				<Toolbar>
-					<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={() => toggleDrawer(true)}>
-						<Menu />
-					</IconButton>
-					<Typography variant="h6" className={classes.title}>
-						Order History
-         			 </Typography>
-				</Toolbar>
-			</AppBar>
-			<SwipeableDrawer
-				anchor="left"
-				open={isDrawerOpen}
-				onClose={() => toggleDrawer(false)}
-				onOpen={() => toggleDrawer(true)}
-			>
-				<List className={classes.list}>
-					<ListItem button onClick={_navigateTo(Path.Index)}>
-						<ListItemIcon><Storefront /></ListItemIcon>
-						<ListItemText primary={"Store"} />
-					</ListItem>
-					<ListItem button onClick={_navigateTo(Path.OrderHistory)}>
-						<ListItemIcon><Receipt /></ListItemIcon>
-						<ListItemText primary={"Order history"} />
-					</ListItem>
-					<ListItem button onClick={_navigateTo(Path.Donate)}>
-						<ListItemIcon><Favorite /></ListItemIcon>
-						<ListItemText primary={"Donate"} />
-					</ListItem>
-				</List>
-			</SwipeableDrawer>
-		</>
-	)
-}
+  const _navigateTo = (localPath: string) => (): void => {
+    history.push(localPath);
+    setIsDrawerOpen(false);
+  };
+
+  return (
+    <>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton
+            edge='start'
+            className={classes.menuButton}
+            color='inherit'
+            aria-label='menu'
+            onClick={() => toggleDrawer(true)}
+          >
+            <Menu />
+          </IconButton>
+          <Typography variant='h6' className={classes.title}>
+            {header.title}
+          </Typography>
+          {header.sideComponent}
+        </Toolbar>
+      </AppBar>
+      <SwipeableDrawer
+        anchor='left'
+        open={isDrawerOpen}
+        onClose={() => toggleDrawer(false)}
+        onOpen={() => toggleDrawer(true)}
+      >
+        <List className={classes.list}>
+          <ListItem button onClick={_navigateTo(Path.Index)}>
+            <ListItemIcon>
+              <Storefront />
+            </ListItemIcon>
+            <ListItemText primary={'Store'} />
+          </ListItem>
+          <ListItem button onClick={_navigateTo(Path.OrderHistory)}>
+            <ListItemIcon>
+              <Receipt />
+            </ListItemIcon>
+            <ListItemText primary={'Order history'} />
+          </ListItem>
+          <ListItem button onClick={_navigateTo(Path.Donate)}>
+            <ListItemIcon>
+              <Favorite />
+            </ListItemIcon>
+            <ListItemText primary={'Donate'} />
+          </ListItem>
+        </List>
+      </SwipeableDrawer>
+    </>
+  );
+};
