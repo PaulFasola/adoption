8154c4ce284213d94f4435877ef3af6f7bec761e
Paul Fasola <paul.fasola@gmail.com>
2021-03-06 23:50:25 -0500

    chore(e-shop): adjust router-paths

--- examples/e-shop/src/router-paths.tsx
+++ examples/e-shop/src/router-paths.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { Donate } from "./views/donate/ìndex";
-import { OrderHistory } from "./views/orderHistory";
-import { Shop } from "./views/shop";
+import React from 'react';
+import { Donate } from './views/donate';
+import { OrderHistory } from './views/orderHistory';
+import { Shop } from './views/shop';
 
 export enum Path {
-	Index = '/',
-	OrderHistory = '/orderhistory',
-	Donate = '/donate'
+  Index = '/',
+  OrderHistory = '/orderhistory',
+  Donate = '/donate',
 }
 
 export const routeMap: Record<Path, React.ReactNode> = {
-	'/': <Shop />,
-	'/orderhistory': <OrderHistory />,
-	'/donate': <Donate />
-}
+  '/': <Shop />,
+  '/orderhistory': <OrderHistory />,
+  '/donate': <Donate />,
+};
