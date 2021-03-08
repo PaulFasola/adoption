ed7fa6e44a880e6c4a733968daeda24775694771
Paul Fasola <paul.fasola@gmail.com>
2021-03-08 03:28:32 -0500

    style(theme): added disabled palette category

--- src/providers/theme/ITheme.ts
+++ src/providers/theme/ITheme.ts
@@ -5,7 +5,10 @@ export interface ITheme {
     rule: string;
     boxShadow: string;
   }>;
+  disabled: Partial<{
+    color: string;
+  }>;
 }
 
-export type PaletteCategory = 'primary' | 'secondary';
+export type PaletteCategory = 'primary' | 'secondary' | 'disabled';
 export type StyleProp = 'backgroundColor' | 'color' | 'boxShadow' | 'rule';
