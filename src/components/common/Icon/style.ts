6da01658ff80aabd8da1dd2dc3601053cc382ddc
Paul Fasola <paul.fasola@gmail.com>
2021-03-09 21:59:41 -0500

    style(icon): swap arrow & onclick hover effect

--- src/components/common/Icon/style.ts
+++ src/components/common/Icon/style.ts
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 import _ from '../../../providers/theme/styleFetcher';
 
+export interface IStyleProps {
+  animate?: boolean;
+  onClick?: () => void;
+}
+
+interface SP extends IStyleProps {}
+
 export const Link = styled.a`
   color: rgba(0, 0, 0, 0.8);
   text-decoration: none;
@@ -11,7 +18,30 @@ export const IconContainer = styled.div`
   display: inline-block;
   vertical-align: middle;
 
+  ${(p: SP) =>
+    typeof p.onClick !== 'undefined' &&
+    `
+    :hover {
+      opacity: 0.6;
+    }
+  `}
+
   svg .fillable {
     fill: ${(p) => _(p.theme, 'primary', 'color')};
   }
+`;
+
+export const ArrowSwap = styled.svg`
+  height: 100%;
+  ${(p: SP) =>
+    p.animate &&
+    `
+    transition: transform  ease-in-out 2s infinite;
+    path:first-of-type {
+      transform: translateY(-55px);
+    }
+    path:nth-child(2){
+      transform: translate3d(0, -50%, 0);
+    }
+  `}
 `;
