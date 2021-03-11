b38101c8b179e9bae2bdb884df23fbca44aac3ed
Paul Fasola <paul.fasola@gmail.com>
2021-03-11 02:14:21 -0500

    style(icon): cleanup, cosmetics, added disabled prop

--- src/components/common/Icon/index.tsx
+++ src/components/common/Icon/index.tsx
@@ -13,6 +13,7 @@ export interface IProps {
   url?: string;
   style?: React.CSSProperties;
   targetBlank?: boolean;
+  disabled?: boolean;
 
   onClick?: () => void;
 }
@@ -48,6 +49,7 @@ export const Icon: React.FC<IProps> = (props) => {
       xmlSpace='preserve'
     >
       <path
+        className='fillable'
         d='M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394
 	l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393
 	C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z'
@@ -58,6 +60,7 @@ export const Icon: React.FC<IProps> = (props) => {
   const _arrowDown: React.FC<IconProps> = () => (
     <svg version='1.1' xmlns='http://www.w3.org/2000/svg' x='0px' y='0px' viewBox='0 0 330 330'>
       <path
+        className='fillable'
         d='M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
 	   c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393
 	   s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z'
@@ -115,10 +118,7 @@ export const Icon: React.FC<IProps> = (props) => {
   }
 
   return (
-    <IconContainer
-      style={props.style}
-      onClick={() => typeof props.onClick === 'function' && props.onClick()}
-    >
+    <IconContainer {...props}>
       <SelectedIcon />
     </IconContainer>
   );
