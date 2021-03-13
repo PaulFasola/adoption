ed411f1faaaba3146b70088478af667e566ec8a6
Paul Fasola <paul.fasola@gmail.com>
2021-03-13 04:44:42 -0500

    docs(protocolselector): storybook initial implementation

--- src/components/ProtocolSelector/index.stories.tsx
+++ src/components/ProtocolSelector/index.stories.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ProtocolSelector } from '.';
+import { ITheme } from '../../providers/theme/ITheme';
+import { ThemeWrapper } from '../../providers/theme/themeWrapper';
+import { IProtocol } from './interfaces';
 import { activeProtocols } from './__tests__/mocks';
 
 console.log(activeProtocols);
@@ -9,5 +12,39 @@ export default {
 };
 
 export const Basic = (): React.ReactNode => {
-  return <ProtocolSelector list={activeProtocols} />;
+  const [activeProtocol, setActiveProtocol] = useState<IProtocol | null>(null);
+
+  const handleChange = (proto: IProtocol | null) => setActiveProtocol(proto);
+
+  return (
+    <>
+      <ProtocolSelector list={activeProtocols} onChange={handleChange} />
+      <div style={{ marginTop: '10px' }}>
+        <p>Selected protocol:</p>
+        <pre>{JSON.stringify(activeProtocol, null, '\t')}</pre>
+      </div>
+    </>
+  );
+};
+
+export const Themed = (): React.ReactNode => {
+  const customThemes: Record<string, Partial<ITheme>> = {
+    ugly: {
+      primary: {
+        hoverColor: 'blue',
+        hoverBgColor: 'green',
+      },
+      defaultButton: {
+        backgroundColor: 'pink',
+        color: 'yellow',
+        hoverBackgroundColor: 'red',
+      },
+    },
+  };
+
+  return (
+    <ThemeWrapper customThemes={customThemes}>
+      <ProtocolSelector list={activeProtocols} />
+    </ThemeWrapper>
+  );
 };
