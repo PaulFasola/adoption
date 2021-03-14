41160a9f3eaa288a367ca4ca702bb12474733a97
Paul Fasola <paul.fasola@gmail.com>
2021-03-13 20:18:54 -0500

    test(jest): ignore dist/ to prevent haste module naming collisions

--- jest.config.js
+++ jest.config.js
@@ -5,15 +5,17 @@ module.exports = {
     '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}',
   ],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/utils/assetsTransformer.js",
-    "\\.(css|less)$": "<rootDir/src/utils/assetsTransformer.js"
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/utils/assetsTransformer.js',
+    '\\.(css|less)$': '<rootDir/src/utils/assetsTransformer.js',
   },
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    'dist/',
-    '<rootDir>/src/index.ts'
+  coveragePathIgnorePatterns: ['/node_modules/', 'dist/', '<rootDir>/src/index.ts'],
+  modulePathIgnorePatterns: ['<rootDir>/dist'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{js,ts,tsx,jsx}',
+    '!<rootDir>/src/**/*.stories.*',
+    '!src/**/style.ts',
   ],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{js,ts,tsx,jsx}', '!<rootDir>/src/**/*.stories.*', '!src/**/style.ts'],
   coverageThreshold: {
     global: {
       branches: 85,
