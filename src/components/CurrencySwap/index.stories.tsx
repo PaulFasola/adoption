6998029981f162b73a7119de5a073686bd8eb6a6
Paul Fasola <paul.fasola@gmail.com>
2021-03-18 03:04:33 -0400

    docs(currencyswap): added payload submission dump

--- src/components/CurrencySwap/index.stories.tsx
+++ src/components/CurrencySwap/index.stories.tsx
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeWrapper } from '../../providers/theme/themeWrapper';
 import { activeProtocols } from '../ProtocolSelector/__tests__/mocks';
 import { CurrencySwap } from '.';
+import { ISwapSubmission } from './interfaces';
 
 export default {
   title: 'Components/CurrencySwap',
 };
 
-export const Basic = (): React.ReactNode => {
+export const Basic = (): React.ReactElement => {
+  const [submittedPayload, setSubmittedPayload] = useState<ISwapSubmission>();
+
   return (
-    <CurrencySwap
-      protocols={{
-        input: activeProtocols,
-      }}
-    />
+    <>
+      <div style={{ display: 'inline-block' }}>
+        <CurrencySwap
+          protocols={{
+            input: activeProtocols,
+          }}
+          onSubmit={(payload) => setSubmittedPayload(payload)}
+        />
+      </div>
+
+      {submittedPayload && (
+        <div style={{ marginTop: '30px' }}>
+          <b>Submission:</b>
+          <pre>{JSON.stringify(submittedPayload, null, '\t')}</pre>
+        </div>
+      )}
+    </>
   );
 };
 
 export const Themed = (): React.ReactNode => {
   return (
     <ThemeWrapper>
-      <CurrencySwap
-        protocols={{
-          input: activeProtocols,
-        }}
-      />
+      <Basic />
     </ThemeWrapper>
   );
 };
