ee03189a1839a30f2b90ae226fcafffaadf62569
Paul Fasola <paul.fasola@gmail.com>
2021-03-19 03:37:30 -0400

    build: export protocolselector, fix rollup build

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './components/TransactionStatus';
 export * from './components/PaymentRequest';
+export * from './components/ProtocolSelector';
 export * from './components/CurrencySwap';
