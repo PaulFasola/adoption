2e6c229a65cf37927e958c4dd0e8b3fea93c5be2
Paul Fasola <paul.fasola@gmail.com>
2021-03-23 21:48:28 -0400

    docs(currencyswap): added controls

--- src/components/CurrencySwap/index.stories.tsx
+++ src/components/CurrencySwap/index.stories.tsx
@@ -1,41 +1,56 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { addDecorator, Meta, Story } from '@storybook/react';
 import { ThemeWrapper } from '../../providers/theme/themeWrapper';
-import { activeProtocols } from '../ProtocolSelector/__tests__/mocks';
 import { CurrencySwap } from '.';
-import { ISwapSubmission } from './interfaces';
+import { IProps } from './interfaces';
+import { withConsole } from '@storybook/addon-console';
+import { activeProtocols } from '../ProtocolSelector/__tests__/mocks';
 
 export default {
   title: 'Components/CurrencySwap',
+  argTypes: {
+    protocols: {
+      input: 'object',
+    },
+  },
+} as Meta;
+
+const args = {
+  protocols: {
+    input: activeProtocols,
+  },
+  locked: false,
+  maxFractionDigits: 5,
+  noShadow: false,
 };
 
-export const Basic = (): React.ReactElement => {
-  const [submittedPayload, setSubmittedPayload] = useState<ISwapSubmission>();
+const Basic: Story<IProps> = (args) => (
+  <>
+    <div style={{ display: 'inline-block' }}>
+      <CurrencySwap
+        onSubmit={action('onSubmit', {
+          clearOnStoryChange: true,
+        })}
+        {...args}
+      />
+    </div>
+    <p>
+      Once clicked on Swap, check the result into the <i>Actions</i> tab below 👇
+    </p>
+  </>
+);
+Basic.args = args;
 
-  return (
-    <>
-      <div style={{ display: 'inline-block' }}>
-        <CurrencySwap
-          protocols={{
-            input: activeProtocols,
-          }}
-          onSubmit={(payload) => setSubmittedPayload(payload)}
-        />
-      </div>
-
-      {submittedPayload && (
-        <div style={{ marginTop: '30px' }}>
-          <b>Submission:</b>
-          <pre>{JSON.stringify(submittedPayload, null, '\t')}</pre>
-        </div>
-      )}
-    </>
-  );
-};
-
-export const Themed = (): React.ReactNode => {
+const Themed: Story<IProps> = (args) => {
   return (
     <ThemeWrapper>
-      <Basic />
+      <Basic {...args} />
     </ThemeWrapper>
   );
 };
+Themed.args = args;
+
+addDecorator((storyFn, context) => withConsole()(storyFn)(context));
+
+export { Basic, Themed };
