fdd06182e0c2a5cc2d2a29e756564c23d00c1f5c
Paul Fasola <paul.fasola@gmail.com>
2021-05-08 03:05:55 -0400

    chore(booleanfield): initial implementation

--- src/components/CurrencySwap/settings/fields/booleanField.tsx
+++ src/components/CurrencySwap/settings/fields/booleanField.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+import { ToggleSwitch } from '../../../common/ToggleSwitch';
 import { IProps } from './fieldBuilder';
 
-export const BooleanField: React.FC<IProps> = () => {
-  return <span>BooleanField</span>;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+
+  & > span {
+    margin-left: 5px;
+  }
+`;
+
+export const BooleanField: React.FC<IProps> = ({ name, onChange, text }) => {
+  const handleChange = (newState: boolean) => {
+    if (typeof onChange !== 'function') {
+      return;
+    }
+
+    onChange(name, newState);
+  };
+
+  return (
+    <Container>
+      <ToggleSwitch id={name} onChange={handleChange} />
+      {text && <span>{text}</span>}
+    </Container>
+  );
 };
